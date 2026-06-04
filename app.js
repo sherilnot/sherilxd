1b87adfcc41f0ece87afed75772f671c32b1a468
sherilnot <sheril4440@gmail.com>
2026-06-04 13:54:26 +0530

    added pari

--- app.js
+++ app.js
@@ -158,4 +158,7 @@ app._router?.stack
 
 console.log('PID route added');
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 module.exports = app;
